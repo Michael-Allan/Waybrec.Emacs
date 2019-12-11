cd367b947a872a65755a0111340508ac2d366ef3
Michael Allan <mike@reluk.ca>
2019-12-10 19:03:34 -0500

    Removal of `let` package wrapper.

--- waybrec-mode.el
+++ waybrec-mode.el
@@ -15,37 +15,36 @@
 
 (require 'breccia-mode)
 
-(let ()
 
 
-  (define-derived-mode waybrec-mode breccia-mode
-    "Waybrec"
-    "A major mode for editing Waybrec.
+(define-derived-mode waybrec-mode breccia-mode
+  "Waybrec"
+  "A major mode for editing Waybrec.
         Home page URL ‘http://reluk.ca/project/wayic/Waybrec/Emacs/’
 User instructions URL ‘http://reluk.ca/project/wayic/Waybrec/Emacs/waybrec-mode.el’"
-    (let* ((bq-pat brec-backquoted-pattern-pattern)
-           (gap brec-gap-pattern)
-           (hc (copy-sequence brec-command-highlighter-components)); So isolating from any other
-             ;;; `breccia-mode` buffer the (deep) changes about to be introduced to this list.
-           (hc-new (last hc 2))); The component after which to insert new components.
+  (let* ((bq-pat brec-backquoted-pattern-pattern)
+         (gap brec-gap-pattern)
+         (hc (copy-sequence brec-command-highlighter-components)); So isolating from any other
+           ;;; `breccia-mode` buffer the (deep) changes about to be introduced to this list.
+         (hc-new (last hc 2))); The component after which to insert new components.
 
-      ;; Patch
-      ;; ─────
-      (push (concat "\\|\\(?1:prepend\\|append\\)" gap "@" gap bq-pat) (cdr hc-new));
-      (push (concat "\\|\\(?1:delete\\|precede\\|succeed\\)" gap bq-pat) (cdr hc-new));
-      (push "\\|\\(?1:replace\\)\\>" (cdr hc-new));
+    ;; Patch
+    ;; ─────
+    (push (concat "\\|\\(?1:prepend\\|append\\)" gap "@" gap bq-pat) (cdr hc-new));
+    (push (concat "\\|\\(?1:delete\\|precede\\|succeed\\)" gap bq-pat) (cdr hc-new));
+    (push "\\|\\(?1:replace\\)\\>" (cdr hc-new));
 
-      ;; Thoroughfractum designator
-      ;; ──────────────────────────
-      (push "\\|\\(?1:thoroughfractum\\)\\>" (cdr hc-new))
+    ;; Thoroughfractum designator
+    ;; ──────────────────────────
+    (push "\\|\\(?1:thoroughfractum\\)\\>" (cdr hc-new))
 
-      (setq-local brec-command-highlighter-components hc))
-        ;;; Locally isolating from any other `breccia-mode` buffer the (shallow) change to this variable.
-    (brec-set-for-buffer 'font-lock-defaults '(brec-keywords)))
+    (setq-local brec-command-highlighter-components hc))
+      ;;; Locally isolating from any other `breccia-mode` buffer the (shallow) change to this variable.
+  (brec-set-for-buffer 'font-lock-defaults '(brec-keywords)))
 
 
 
-  (provide 'waybrec-mode))
+(provide 'waybrec-mode)
 
 
                                        ;;; Copyright © 2019 Michael Allan and contributors.  Licence MIT.
